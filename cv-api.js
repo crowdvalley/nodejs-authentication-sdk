91b4dc0e1af3d48fbd9676bc62029bf4d6b4093e
dpischalnikov <pischalnikov.dv@gmail.com>
2016-12-13 22:24:01 +0600

    feature/ALLOW_NON_ASCII_PASSWORDS added ability to use symbols like ääööää in passwords

--- cv-api.js
+++ cv-api.js
@@ -2,15 +2,37 @@ var crypto = require('crypto');
 
 var exports = module.exports = {};
 
+function chr (codePt) {
+
+  if (codePt > 0xFFFF) { // Create a four-byte string (length 2) since this code point is high
+    //   enough for the UTF-16 encoding (JavaScript internal use), to
+    //   require representation with two surrogates (reserved non-characters
+    //   used for building other characters; the first is "high" and the next "low")
+    codePt -= 0x10000
+    return String.fromCharCode(0xD800 + (codePt >> 10), 0xDC00 + (codePt & 0x3FF))
+  }
+  return String.fromCharCode(codePt)
+}
+
 function _encrypt (secretKey, sourcePassword, targetLen) {
     // Our plaintext/ciphertext
-    var text = secretKey.toString();
+
+    var bytes = [];
+    var charCode;
+
+    var myBuffer = [];
+    var str = secretKey;
+    var buffer = new Buffer(secretKey, 'utf8');
+    for (var i = 0; i < buffer.length; i++) {
+        myBuffer.push(buffer[i]);
+    }
+
     // Our output text
     var outText = '';
     // Iterate through each character
-    for(var i = 0; i < text.length; i) {
-      for(var j = 0; ( j < sourcePassword.length && i < text.length ); j++, i++) {
-          outText += String.fromCharCode(text[i].charCodeAt(0)^sourcePassword[j].charCodeAt(0));
+    for(var i = 0; i < myBuffer.length; i) {
+      for(var j = 0; ( j < sourcePassword.length && i < myBuffer.length ); j++, i++) {
+          outText += chr(myBuffer[i]^sourcePassword[j].charCodeAt(0));
       }
     }
     return outText;
@@ -39,12 +61,10 @@ function _createNonce(nonceLen) {
 function _createToken(apiKey,apiSecret,userName,userPass) {
 
     var nonce = _createNonce(10);
-
-    var userPass64 = new Buffer(userPass).toString('base64')
+    var userPass64 = new Buffer(userPass, 'ascii').toString('base64')
     var created = _getW3CDate(new Date());
     var sha1Sum = crypto.createHash('sha1');
     sha1Sum.update(nonce + created + apiSecret);
-
     var digest = sha1Sum.digest('base64');
 
     var token = "AuthToken ApiKey=\"" + apiKey + "\", TokenDigest=\"" + digest + "\", Nonce=\""+ nonce + "\", Created=\"" + created + "\", Username=\"" + userName + "\", Password=\"" + userPass64 + "\"";
